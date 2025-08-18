077bdda7235d600f8ab4f1b0f54f90b8afdeec70
Fedora <emily.wong@unige.ch>
2025-08-18 22:40:15 +0200

    First draft to share

--- makepdf.sh
+++ makepdf.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 pdflatex enceladus_crater.tex
-bibtex enceladus_crater
-pdflatex enceladus_crater.tex
-pdflatex enceladus_crater.tex
+#bibtex enceladus_crater
+#pdflatex enceladus_crater.tex
+#pdflatex enceladus_crater.tex
+
+#pdflatex new.tex
+#bibtex enceladus_crater
+#pdflatex new.tex
+#pdflatex new.tex
