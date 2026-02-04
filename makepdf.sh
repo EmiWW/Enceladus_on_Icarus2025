36769952a922ee029ba631cae8c61c06abdfe11b
Fedora <emily.wong@unige.ch>
2026-02-04 17:06:54 +0100

    Trimmed until the last subsection of the "Method"
    TODO:
    - Add more description on how to construct the CPF by scaling and linking the crater size-frequency measurement
       MAIN POINT: why the construction of CPF is justified.
    - continue to trim the "Result" and rest
    - Need to reconsider the very introductory paragraph
    - Need to reduce the length of the previous studies
    - Need to repolot some figures
    - Consider if it's still need to show all Crater size-frequency
      measurement by grouping together some units with similiar distribution
      and crater density
    
    Copy form the "last TODO on Nov3 commit"
        last TODO:
        - edit 90-deg on map to 0-deg
        - add the units coordinate
        - discuss unit or geological unit

--- makepdf.sh
+++ makepdf.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-pdflatex enceladus_crater.tex
-bibtex enceladus_crater
-pdflatex enceladus_crater.tex
-pdflatex enceladus_crater.tex
+
+if ["$1" = "full" ]; then
+  pdflatex enceladus_crater.tex
+  bibtex enceladus_crater
+  pdflatex enceladus_crater.tex
+  pdflatex enceladus_crater.tex
+else
+  pdflatex enceladus_crater.tex
+fi
 
 #pdflatex new.tex
 #bibtex enceladus_crater
