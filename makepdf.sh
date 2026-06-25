f909aebb5fa45a45715002d725030c890e101fdf
Fedora <emily.wong@unige.ch>
2026-06-25 17:49:08 +0200

    complete ~50% of first reviewer's comment

--- makepdf.sh
+++ makepdf.sh
@@ -5,13 +5,14 @@ if [ "$1" = "full" ]; then
   bibtex enceladus_crater
   pdflatex enceladus_crater.tex
   pdflatex enceladus_crater.tex
+  cp enceladus_crater.pdf latest_copy.pdf
+elif [ "$1" = "diff" ]; then
+  latexdiff first_submission.tex enceladus_crater.tex >diff.tex
+  pdflatex diff.tex
+  bibtex diff
+  pdflatex diff.tex
+  pdflatex diff.tex
 else
   pdflatex enceladus_crater.tex
+  cp enceladus_crater.pdf latest_copy.pdf
 fi
-
-cp enceladus_crater.pdf latest_copy.pdf
-
-#pdflatex new.tex
-#bibtex enceladus_crater
-#pdflatex new.tex
-#pdflatex new.tex
