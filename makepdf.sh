c27098556e3e2568c1be45052b75f5c3cec9f935
Fedora <emily.wong@unige.ch>
2026-07-14 22:32:41 +0200

    TODO: change Fig 2 and 3a with AI. Generate new Fig S3. Ready to sent to co-author

--- makepdf.sh
+++ makepdf.sh
@@ -7,8 +7,9 @@ if [ "$1" = "full" ]; then
   pdflatex enceladus_crater.tex
   cp enceladus_crater.pdf latest_copy.pdf
 elif [ "$1" = "diff" ]; then
-  #latexdiff first_submission.tex enceladus_crater.tex >diff.tex
-  latexdiff --append-safecmd=url first_submission_edit.tex enceladus_crater.tex >diff.tex
+  latexdiff first_submission.tex enceladus_crater.tex >diff.tex
+  #latexdiff --append-safecmd=url first_submission_edit.tex enceladus_crater.tex >diff.tex
+  #latexdiff --append-textcmd="DIFadd" first_submission_edit.tex enceladus_crater.tex >diff.tex
   echo "diff.text has been made"
   pdflatex diff.tex
   bibtex diff
