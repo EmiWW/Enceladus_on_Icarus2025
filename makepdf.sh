e4508c1979fa4d6e8c82087f8fa8ea57ba71cd00
Fedora <emily.wong@unige.ch>
2026-07-20 20:23:49 +0200

    ready to be resubmitted version

--- makepdf.sh
+++ makepdf.sh
@@ -5,18 +5,23 @@ if [ "$1" = "full" ]; then
   bibtex enceladus_crater
   pdflatex enceladus_crater.tex
   pdflatex enceladus_crater.tex
-  cp enceladus_crater.pdf latest_copy.pdf
-elif [ "$1" = "diff" ]; then
-  latexdiff first_submission.tex enceladus_crater.tex >diff.tex
+  cp enceladus_crater.pdf revision.pdf
+elif [ "$1" = "difffull" ]; then
+  latexdiff first_submission_edit.tex enceladus_crater.tex >diff.tex
   #latexdiff --append-safecmd=url first_submission_edit.tex enceladus_crater.tex >diff.tex
   #latexdiff --append-textcmd="DIFadd" first_submission_edit.tex enceladus_crater.tex >diff.tex
-  echo "diff.text has been made"
   pdflatex diff.tex
   bibtex diff
   pdflatex diff.tex
   pdflatex diff.tex
-  cp diff.pdf latest_diff.pdf
+  cp diff.pdf difference.pdf
+elif [ "$1" = "diff" ]; then
+  latexdiff first_submission_edit.tex enceladus_crater.tex >diff.tex
+  #latexdiff --append-safecmd=url first_submission_edit.tex enceladus_crater.tex >diff.tex
+  #latexdiff --append-textcmd="DIFadd" first_submission_edit.tex enceladus_crater.tex >diff.tex
+  pdflatex diff.tex
+  cp diff.pdf difference.pdf
 else
   pdflatex enceladus_crater.tex
-  cp enceladus_crater.pdf latest_copy.pdf
+  cp enceladus_crater.pdf revision.pdf
 fi
